20cff35f90bab1ddb67dcb0fb7f00c4fd6c8c216
Vinicius Noda <viniciusnoda@alunos.utfpr.edu.br>
2016-09-07 15:30:18 -0300

    Delete CreateUI
    CreateUI deletada, métodos passados para a EngSoft.

--- src/engsoft/EngSoft.java
+++ src/engsoft/EngSoft.java
@@ -5,15 +5,23 @@
  */
 package engsoft;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 
 /**
  *
  * @author Gabriel
  */
-public class EngSoft{
+public class EngSoft extends Application{
     
     public static void main(String[] args) {
-        CreateUI.launch(args); //Classe main para o start do programa       
+        EngSoft.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ControleUI.getInstance().start(primaryStage); //Chama a inicialização da classe de controle
     }
     
 }
