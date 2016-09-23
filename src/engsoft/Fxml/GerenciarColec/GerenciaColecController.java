9b3f792dd536bfee3d6dcad04b081f3e1a52e97a
Vinícius Noda <viniciusnoda@alunos.utfpr.edu.br>
2016-09-23 11:54:17 -0300

    Alterações Estruturais
    Uma view em cada pasta.

--- src/engsoft/Fxml/GerenciarColec/GerenciaColecController.java
+++ src/engsoft/Fxml/GerenciarColec/GerenciaColecController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package engsoft.Fxml;
+package engsoft.Fxml.GerenciarColec;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/engsoft/Fxml/RecuperaSenha/RecuperaSenhaController.java
+++ src/engsoft/Fxml/RecuperaSenha/RecuperaSenhaController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package engsoft.Fxml;
+package engsoft.Fxml.RecuperaSenha;
 
 import engsoft.Valida;
 import java.net.URL;
