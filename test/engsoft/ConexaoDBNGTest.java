f4360fead01d9333e3cc4127526682dd4b071b3a
LucasTsuchiya <lucastsuchiya@alunos.utfpr.edu.br>
2016-10-01 12:12:39 -0300

    Casos de Teste ConexaoDBNGTTeste
    Foi realizado teste na classe Teste ConexaoDBNGT

---
+++ test/engsoft/ConexaoDBNGTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engsoft;
+
+import java.sql.Connection;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Lucas B. Tsuchiya
+ */
+public class ConexaoDBNGTest {
+    
+    public ConexaoDBNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of getCon method, of class ConexaoDB.
+     */
+
+
+    /**
+     * Test of realizaCadastro method, of class ConexaoDB.
+     */
+    @Test
+    public void testRealizaCadastroCerto() {
+        System.out.println("realizaCadastro");
+        String NickField = "celularolaoi";
+        String NomeField = "Lucas Tsuchiya";
+        String EndField = "Avenida padre Paulo broda";
+        String NumField = "19";
+        String EmailField = "patricia3@gmail.com";
+        String PaisField = "Brasil";
+        String EstadoField = "PR";
+        String CityField = "Londrina";
+        String PassField = "lucas12vinho";
+        ConexaoDB instance = new ConexaoDB();
+        String expResult = "Usuário Cadastrado com sucesso!";
+        String result = instance.realizaCadastro(NickField, NomeField, EndField, NumField, EmailField, PaisField, EstadoField, CityField, PassField);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail
+    }
+    
+        @Test
+    public void testRealizaCadastroRepetido() {
+        System.out.println("realizaCadastro");
+        String NickField = "celular";
+        String NomeField = "Lucas Tsuchiya";
+        String EndField = "Avenida padre Paulo broda";
+        String NumField = "19";
+        String EmailField = "patricia123@gmail.com";
+        String PaisField = "Brasil";
+        String EstadoField = "PR";
+        String CityField = "Londrina";
+        String PassField = "lucas12vinho";
+        ConexaoDB instance = new ConexaoDB();
+        String expResult = "Nome de usuário ou email já  cadastrado!";
+        String result = instance.realizaCadastro(NickField, NomeField, EndField, NumField, EmailField, PaisField, EstadoField, CityField, PassField);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail
+    }
+    @Test
+    public void testAltSenha() {
+        System.out.println("altSenha");
+        String NickField = "celular";
+        String PassField = "lucasvinho";
+        ConexaoDB instance = new ConexaoDB();
+        String expResult = "Alteração executada com sucesso!";
+        String result = instance.altSenha(NickField, PassField);
+        assertEquals(result, expResult);
+    }
+    
+        @Test
+    public void testAltSenhanencotrado() {
+        System.out.println("altSenha");
+        String NickField = "celulartelefone";
+        String PassField = "lucasvinho";
+        ConexaoDB instance = new ConexaoDB();
+        String expResult = "Usuário não encontrado!";
+        String result = instance.altSenha(NickField, PassField);
+        assertEquals(result, expResult);
+    }
+   
+   
+}
